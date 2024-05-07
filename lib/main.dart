79a9e5259bd08cac01a11c4d45a406c24fa6f33b
unknown <yonideserbaix@gmail.com>
2024-05-08 00:12:23 +0200

    feat(main.dart): create 2nd page

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Flutter App",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/images/flutter-bird-logo.png"),
-            const Text(
-              "My First Flutter App",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Poppins"),
-            ),
-            Text(
-              "lorem Ipsum is simply dummy text of the printing",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-            )
-          ],
-        )),
-      ),
+      home: EventPage(),
     );
   }
+}
+
+class EventPage extends StatelessWidget {
+  const EventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text(
+            "Planning",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: const Center(
+          child: Text("Available soon", style: TextStyle(fontSize: 25)),
+        ));
+  }
+}
+
+Widget HomePage() {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text(
+        "Flutter App",
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.blue,
+    ),
+    body: Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/flutter-bird-logo.png"),
+        const Text(
+          "First Flutter App",
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, fontFamily: "Poppins"),
+        ),
+        Text(
+          "lorem Ipsum is simply dummy text of the printing",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+          textAlign: TextAlign.center,
+        )
+      ],
+    )),
+  );
 }

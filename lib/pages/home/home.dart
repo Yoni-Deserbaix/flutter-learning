53a544f1f693710394558b0953182def52bfd97b
unknown <yonideserbaix@gmail.com>
2024-05-07 20:51:55 +0200

    feat(home.dart): move image in left hand corner

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -9,7 +9,12 @@ class HomePage extends StatelessWidget {
       backgroundColor: Color(0xFF5F67EA),
       body: SingleChildScrollView(
         child: Stack(
-          children: [Image.asset("assets/images/bg_liquid.png", width: 200)],
+          children: [
+            Transform(
+                transform: Matrix4.identity()..rotateZ(20),
+                origin: const Offset(150, 50),
+                child: Image.asset("assets/images/bg_liquid.png", width: 200))
+          ],
         ),
       ),
     );

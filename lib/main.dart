74ca29f495250e5589954db760e979dba492278f
unknown <yonideserbaix@gmail.com>
2024-05-08 09:50:21 +0200

    feat(main.dart): create redirection function to the button

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -38,48 +38,62 @@ class EventPage extends StatelessWidget {
   }
 }
 
-Widget HomePage() {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text(
-        "Flutter App",
-        style: TextStyle(color: Colors.white),
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Flutter App",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      backgroundColor: Colors.blue,
-    ),
-    body: Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset("assets/images/flutter-bird-logo.png"),
-        const Text(
-          "First Flutter App",
-          style: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, fontFamily: "Poppins"),
-        ),
-        const Text(
-          "lorem Ipsum is simply dummy text of the printing",
-          style: TextStyle(
-            fontSize: 20,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/flutter-bird-logo.png"),
+          const Text(
+            "First Flutter App",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Poppins"),
           ),
-          textAlign: TextAlign.center,
-        ),
-        const Padding(padding: EdgeInsets.only(top: 10)),
-        ElevatedButton.icon(
-          style: const ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(Colors.blue),
-              padding: MaterialStatePropertyAll(EdgeInsets.all(15))),
-          onPressed: () => print("click btn"),
-          label: const Text(
-            "Display the planning",
-            style: TextStyle(fontSize: 15, color: Colors.white),
+          const Text(
+            "lorem Ipsum is simply dummy text of the printing",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
           ),
-          icon: const Icon(
-            Icons.calendar_month,
-            color: Colors.white,
-          ),
-        )
-      ],
-    )),
-  );
+          const Padding(padding: EdgeInsets.only(top: 10)),
+          ElevatedButton.icon(
+            style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                padding: MaterialStatePropertyAll(EdgeInsets.all(15))),
+            onPressed: () {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const EventPage(),
+                ),
+              );
+            },
+            label: const Text(
+              "Display the planning",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+            icon: const Icon(
+              Icons.calendar_month,
+              color: Colors.white,
+            ),
+          )
+        ],
+      )),
+    );
+  }
 }
